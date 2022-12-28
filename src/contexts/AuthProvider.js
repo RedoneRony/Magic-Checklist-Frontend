cdfed46d838feaea6992773f9c805bc0359ea24a
MdRedone <redone.hassan@sjinnovation.com>
2022-12-28 15:22:44 +0600

    changes in auth provider file

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,72 +1,74 @@
-import React, { createContext, useEffect, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth'
-import app from '../firebase/firebase.config';
+import React, { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    // Provider login 
-    const providerLogin = (provider) => {
-        setLoading(true)
-        return signInWithPopup(auth, provider);
-    }
+  // Provider login
+  const providerLogin = (provider) => {
+    setLoading(true);
+    return signInWithPopup(auth, provider);
+  };
 
-    // Email and password register 
-    const createUser = (email, password) => {
-        setLoading(true)
-        return createUserWithEmailAndPassword(auth, email, password)
-    }
+  // Email and password register
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-    // Email and password login 
-    const signIn = (email, password) => {
-        setLoading(true)
-        return signInWithEmailAndPassword(auth, email, password);
-    }
+  // Email and password login
+  const signIn = (email, password) => {
+    setLoading(false);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-    const updateUserInfo = (profile) => {
-        return updateProfile(auth.currentUser, profile);
-    }
+  const updateUserInfo = (profile) => {
+    return updateProfile(auth.currentUser, profile);
+  };
 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser);
-            setLoading(false)
-
-        });
-
-        return () => {
-            unsubscribe();
-        }
-
-    }, [])
-
-    // LogOut 
-    const logOut = () => {
-        setLoading(true)
-        return signOut(auth);
-    }
-
-
-    const authInfo = {
-        user,
-        providerLogin,
-        createUser,
-        signIn,
-        logOut,
-        loading,
-        updateUserInfo
+    return () => {
+      unsubscribe();
     };
+  }, []);
 
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+  // LogOut
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  const authInfo = {
+    user,
+    providerLogin,
+    createUser,
+    signIn,
+    logOut,
+    loading,
+    updateUserInfo,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
